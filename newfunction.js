9c649ab29e699b498938ba4364ffb91920ead9e6
Yoonhee Son <dev_uni77@gmail.com>
2025-02-11 21:52:11 +0900

    done

---
+++ newfunction.js
@@ -0,0 +1,31 @@
+
+
+function HouseKeeper(experience,name,cleaning {
+    this.yearsofeperience = experience;
+
+    this.name = name;
+    this.cleaning_perference = cleaning;
+})
+
+
+
+var housekeepr1 = new HouseKepper(9,"MH, "bed");
+
+
+    function cando() {
+        stair;
+        kitchen;
+    }
+
+
+
+function HouseKeeper(experience,name,cleaning) {
+    this.yearsofeperience = experience;
+    this.name = name;
+    this.cleaning_perference = cleaning;
+    this.cando = function() {
+        alert("Cleaning in progree....")
+    }
+})
+
+var housekeepr1 = new Housekeepr ( 12, "OMH", "toilet")
